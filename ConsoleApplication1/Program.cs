abff0274a2aba2f27c8afb3c97370cf2ecc84098
Doug Evenhouse <doug@evenhouseconsulting.com>
2015-12-16 18:54:02 -0600

    Adds concole app to generate encryption key and updates some dependencies

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using pgProvider;
+
+namespace GenEncryptionKeyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Encryption.GenerateAESKey().ToBase64());
+            Console.WriteLine("Hit Enter to end.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GenEncryptionKeyConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using pgProvider;
+
+namespace GenEncryptionKeyConsole
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine(Encryption.GenerateAESKey().ToBase64());
+            Console.WriteLine("Hit Enter to end.");
+            Console.ReadLine();
+        }
+    }
+}
